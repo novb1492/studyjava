cb909d9693fa2cfd8daa73f5c3801b72cb42971b
novb1492 <novb1492@naver.com>
2021-04-19 14:20:40 +0900

    test2

---
+++ studyclass.java
@@ -0,0 +1,13 @@
+class function{
+ public void print()
+ {
+    System.out.println("helloworld");
+ }
+}
+public class studyclass {
+    public static void main(String[] args) {
+        function f=new function();
+        f.print();
+        
+    }
+}
