ae5b54003e25297aa95e7ac4271eb20a2eaded29
novb1492 <novb1492@naver.com>
2021-04-19 22:24:54 +0900

    likerpg

--- hello/src/studyconstructor.java
+++ hello/src/studyconstructor.java
@@ -1,22 +1,61 @@
 package hello.src;
+
+import java.util.Scanner;
+
 class p{
     int hp;
-    
+    int attack;
     public void showhp()
     {
         System.out.println(this.hp+"hp");
     }
+    public void showattack()
+    {
+        System.out.println(this.attack+"attack");
+    }
+    public void skillone(){}
 }
 class thor extends p{
   public thor()
   {
       hp=50;
+      attack=100;
   }
+  @Override
+  public void skillone() {
+      System.out.println("throwhammer");
+      super.skillone();
+  }
+}
+class ironman extends p{
+    public ironman()
+    {
+        hp=30;
+        attack=80;
+    }
+    @Override
+    public void skillone() {
+        System.out.println("bim");
+        super.skillone();
+    }
 }
 public class studyconstructor {
     public static void main(String[] args) {
-     p p=new thor();
+        p p;
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a==1)
+        {
+            p=new thor();
+        }
+        else
+        {
+            p=new ironman();
+
+        }
         p.showhp();
+        p.showattack();
+        p.skillone();
     }
     
 }
