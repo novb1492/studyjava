fe12f05eb24b0df433ead1723d4c5d673b00cdbe
novb1492 <novb1492@naver.com>
2021-04-21 21:45:28 +0900

    impot/override/class/상속/패키지/예외처리/어느정도 이해 완료!

--- javarpg/src/App.java
+++ javarpg/src/App.java
@@ -2,20 +2,33 @@
 import character.*;
 import java.util.Scanner;
 
+
+
 public class App {
     public static void main(String[] args) throws Exception {
      
         unit u;
       Scanner sc= new Scanner(System.in);
-      if(sc.nextInt()==1)
-      {
-          u=new thor();
+      int a=sc.nextInt();
+      try { 
+        if(a==1)
+        {
+            u=new thor();
+        }
+        else if(a==2)
+        {
+            u=new ironman();
+        }
+        else{
+            u=null;/////////요게없다면 
+        }
+        u.printfhp();///////////이게아예요류가남java는 만약의 상황까지 대비해서 똑똑한거같다,2021-04-21
+        u.skillone();
+          
+      } catch (Exception e) {
+          System.out.println("잘못된 직업 선택"+e.getMessage());
       }
-      else
-      {
-          u=new ironman();
-      }
-      u.printfhp();
-      u.skillone();
+      ////////////////c/c++보다 어려운거같다 포인터는 없지만 import나 여러가지 이해하는데 조금 걸렸다 2021-04-21
+      ////개인적으로 아직 c++이더 좋은거 같다 ㅋㅋㅋ
     }
 }
